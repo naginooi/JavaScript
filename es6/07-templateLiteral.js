7d974335752a3f5d52a482461f1e59a87ad08b46
naginooi <xxxkmsz@naver.com>
2023-09-07 14:12:59 +0900

    es6 commit~

---
+++ es6/07-templateLiteral.js
@@ -0,0 +1,14 @@
+/** ES6 template literal */
+let name6 = "홍길동6";
+let greeting6 = `Hello my name is ${name6}`; //백틱(backtick) 사용
+
+console.log(greeting6); //Hello my name is 홍길동6
+
+let a6 = 1;
+let b6 = 10;
+console.log(`1 * 10 = ${a6 * b6}`); //1 * 10 = 10
+
+const text6 = `hello,
+my name is 홍길동
+how are you? `;
+console.log(text6);
