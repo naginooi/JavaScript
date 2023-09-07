e994d75dcde5bc6d909030756531c7de4132eb10
naginooi <xxxkmsz@naver.com>
2023-09-07 14:09:18 +0900

    es6 commit~

---
+++ es6/06-defaultValue.js
@@ -0,0 +1,27 @@
+/** ES5 function default value */
+function getLocation(city, country, continent) {
+  if (typeof country === 'undefined') {
+    country = 'Italy';
+  }
+  if (typeof continent === 'undefined') {
+    continent = 'Europe';
+  }
+  console.log(continent, country, city);
+}
+
+getLocation('Milano');
+getLocation('Paris', 'France');
+console.log("\n");
+//Europe Italy Milano
+//Europe France Paris
+
+/** ES6 function default value */
+function getLocation2(city, country = 'Italy', continent = 'Europe') {
+  console.log(continent, country, city);
+  //Europe Italy Milano
+  //Europe France Paris
+}
+
+getLocation2('Milano');
+getLocation2('Paris', 'France');
+
