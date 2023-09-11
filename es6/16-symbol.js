e5163fb1cf716a3dcaf386569c160b0731f8044c
naginooi <xxxkmsz@naver.com>
2023-09-11 13:56:55 +0900

    symbol

--- es6/16-symbol.js
+++ es6/16-symbol.js
@@ -1,11 +1,15 @@
 /** 심벌의 고유성 */
-const me = Symbol("Alberto");
-console.log(me);
+//원시데이터타입을 상수형태로 만들어주는 기능
+const me = Symbol("Alberto"); 
+console.log(me); //Symbol(Alberto)
 
 /** 같은 값을 가진 새로운 심벌을 만들면 어떻게 될까? */
 const clone = Symbol("Alberto");
-console.log(clone);
+console.log(clone); //Symbol(Alberto)
 
 //심벌 비교
 console.log(me == clone);   //false
 console.log(me === clone);  //false
+
+const symObj = Object(me) 
+console.log(symObj); //[Symbol: Symbol(Alberto)]
--- es6/16-symbol2.js
+++ es6/16-symbol2.js
@@ -1,33 +1,16 @@
-/** 객체 속성에 대한 식별자 */
-const office = {
-  "Tom": "CEO",
-  "Mark": "CTO",
-  "Mark": "CIO",
-};
-for (person in office) {
-  console.log(person);
-}
-
-const office2 = {
-  [Symbol("Tom")]: "CEO",
-  [Symbol("Mark")]: "CTO",
-  [Symbol("Mark")]: "CIO",
-};
-for (person in office2) {
-  console.log(person);
-}
-
 /** 객체 속성에 대한 식별자 얻어오기 */
-const office3 = {
+const office2 = { 
   [Symbol("Tom")]: "CEO",
   [Symbol("Mark")]: "CTO",
   [Symbol("Mark")]: "CIO",
-};
+}; // [] : 심볼값을 객체로 받아오려고 리스트화
+
+console.log(office2); //{ [Symbol(Tom)]: 'CEO', [Symbol(Mark)]: 'CTO', [Symbol(Mark)]: 'CIO' }
 
 //office 객체의 symbol 속성을 배열로 얻어오기
-const symbolList = Object.getOwnPropertySymbols(office3);
-console.log(symbolList);
+const symbolList = Object.getOwnPropertySymbols(office2);
+console.log(symbolList); //[ Symbol(Tom), Symbol(Mark), Symbol(Mark) ]
 
 //속성 배열을 이용하여 속성 값 가져오기
-const value = symbolList.map(symbol => office3[symbol]);
-console.log(value);
+const value = symbolList.map(symbol => office2[symbol]);
+console.log(value); //[ 'CEO', 'CTO', 'CIO' ]
